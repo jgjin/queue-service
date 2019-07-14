d6ec6ef632da8069cee1d5bf54cdb1971976ec99
banana <jgjin@umich.edu>
2019-07-14 16:41:33 -0700

    queue api?

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,31 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
 extern crate crossbeam_queue;
+extern crate periodic;
+extern crate reqwest;
 #[macro_use] extern crate rocket;
 #[macro_use] extern crate rocket_contrib;
 extern crate serde;
 extern crate serde_json;
 
+mod mock;
+
 use std::{
-    thread,
+    sync::{
+        Arc,
+        RwLock,
+    },
+    time::{
+        Duration,
+    },
 };
 
 use crossbeam_queue::{
     SegQueue,
+};
+use periodic::{
+    Planner,
+    Every,
 };
 use rocket::{
     State,
@@ -28,6 +42,11 @@ use serde::{
 };
 
 #[derive(Debug, Deserialize, Serialize)]
+struct TheodorResponse {
+    time_remaining: i32,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
 enum MachineState {
     On,
     Off,
@@ -38,16 +57,59 @@ struct User {
     name: String,
 }
 
+fn get_time_rem(
+) -> reqwest::Result<TheodorResponse> {
+    reqwest::get("http://localhost:8000/mock_time_rem")?
+        .json()
+}
+
 fn popper(
-    queue: SegQueue<User>,
-) -> {
-    thread
+    planner: &mut Planner,
+    queue: Arc<SegQueue<User>>,
+) {
+    let machine_state: Arc<RwLock<MachineState>> = Arc::new(RwLock::new(
+        MachineState::Off,
+    ));
+    let queue_clone = queue.clone();
+
+    planner.add(
+        move || {
+            get_time_rem().map(|theodor_response| {
+                let tr = theodor_response.time_remaining;
+                println!("{} {:?}", tr, machine_state);
+                // *(
+                //     machine_state.write().expect("poisoned RwLock")
+                // ) = match *(
+                //     machine_state.read().expect("poisoned RwLock")
+                // ) {
+                //     MachineState::Off => Some(tr).filter(|tr| {
+                //         *tr > 0
+                //     }).and_then(|_| {
+                //         queue_clone.pop().map(|_| {
+                //             MachineState::On
+                //         }).ok()
+                //     }).or_else(|| {
+                //         Some(MachineState::Off)
+                //     }).unwrap(),
+                //     MachineState::On => Some(tr).filter(|tr| {
+                //         *tr < 0
+                //     }).and_then(|_| {
+                //         Some(MachineState::Off)
+                //     }).or_else(|| {
+                //         Some(MachineState::On)
+                //     }).unwrap(),
+                // };
+                println!("{}_{:?}", tr, machine_state);
+            }).expect("error in theodor api");
+        },
+        Every::new(Duration::from_secs(2)),
+    );
 }
 
 #[post("/push", format = "application/json", data = "<user>")]
 fn push(
     user: Json<User>,
-    queue: State<SegQueue<User>>,
+    queue: State<Arc<SegQueue<User>>>,
 ) -> JsonValue {
     let user = user.into_inner();
     queue.push(user.clone());
@@ -59,7 +121,7 @@ fn push(
 
 // #[get("/pop", format = "application/json")]
 // fn pop(
-//     queue: State<SegQueue<User>>,
+//     queue: State<Arc<SegQueue<User>>>,
 // ) -> JsonValue {
 //     queue.pop().map(|user| {
 //         json!({
@@ -74,7 +136,7 @@ fn push(
 
 #[get("/get", format = "application/json")]
 fn get(
-    queue: State<SegQueue<User>>,
+    queue: State<Arc<SegQueue<User>>>,
 ) -> JsonValue {
     let mut vec = Vec::new();
 
@@ -91,7 +153,7 @@ fn get(
 }
 
 fn rocket(
-    queue: SegQueue<User>,
+    queue: Arc<SegQueue<User>>,
 ) -> rocket::Rocket {
     rocket::ignite()
         .mount("/", routes![
@@ -99,11 +161,19 @@ fn rocket(
             // pop,
             get,
         ])
+        .mount("/mock_time_rem", routes![
+            mock::mock_time_rem,
+        ])
         .manage(queue)
 }
 
 fn main(
 ) {
-    let queue = SegQueue::new();
+    let mut planner = Planner::new();
+    let queue = Arc::new(SegQueue::new());
+
+    popper(&mut planner, queue.clone());
+
+    planner.start();
     rocket(queue).launch();
 }
